20a9025da3f2bed4a1791db4a623da06f92b6fcd
fayez <fayezbast15@gmail.com>
2024-12-11 20:49:09 +0000

    1

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -2,13 +2,17 @@
 
 /**
  * main - Entry point
- * Description: Prints "_putchar" followed by a new line
+ *
+ * Description: Prints "_putchar" followed by a new line.
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
     char str[] = "_putchar\n";
+
     write(1, str, sizeof(str) - 1);
+
     return (0);
 }
-
+i
