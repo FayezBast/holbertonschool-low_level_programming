893eef59e20e9e27e48058d0104e7bc4f1301010
fayez <fayezbast15@gmail.com>
2024-12-11 21:30:01 +0000

    2

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -12,8 +12,8 @@ void print_alphabet(void)
 
     for (letter = 'a'; letter <= 'z'; letter++)
     {
-        putchar('letter');
+       _ putchar(letter);
     }
-    putchar('\n');
+    _putchar('\n');
 }
 
