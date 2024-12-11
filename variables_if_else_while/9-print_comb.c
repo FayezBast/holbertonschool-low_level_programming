6fcf293ff1582bcad0752b1c2e46e3008e9db56a
fayez <fayezbast15@gmail.com>
2024-12-11 18:57:23 +0000

    9

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -5,7 +5,6 @@
  *
  * Return: Always 0.
  */
-
 int main(void)
 {
 	int num;
@@ -15,13 +14,9 @@ int main(void)
 		putchar((num % 10) + '0');
 		if (num == 9)
 			continue;
-		
 		putchar(',');
 		putchar(' ');
 	}
-
 	putchar('\n');
-
 	return (0);
 }
-
