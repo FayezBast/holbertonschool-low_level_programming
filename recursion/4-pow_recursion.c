2fefd84d628d1d813b503dbeae4db086e412b879
fayez bast <fayezbast15@gmail.com>
2025-01-06 14:05:46 +0000

    power using rec

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -1,22 +1,23 @@
 #include "main.h"
-
 /**
- * factorial - Write a function that returns the power  of a given number.
+ * _pow_recursion - Write a function that returns the value of x raised to the
+ * power of y.
  *
- * @n: This is the input number
+ * @x: This is the input number
+ * @y: This is the power number
  *
- * Return: The value of a given number raised to another
+ * Return: if "y" is bigger than zero raised to the power of "y",
+ * return -1 if "y" is lower than zero, return 1 if "y" is equal to 1, 
  */
-int  _pow_recursion(int x, int y)
+int _pow_recursion(int x, int y)
 {
+	if (y == 0)
+	{
+		return (1);
+	}
 	if (y < 0)
 	{
 		return (-1);
 	}
-	if (y=0)
-	{
-		return(1);
-	}
 	return (x * _pow_recursion(x, y - 1));
-
 }
