6aadd627689e96a22d2af0a8e020ece1c1973f0a
fayez bast <fayezbast15@gmail.com>
2025-01-06 13:49:45 +0000

    power using rec

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * factorial - Write a function that returns the power  of a given number.
+ *
+ * @n: This is the input number
+ *
+ * Return: The value of a given number raised to another
+ */
+int  _pow_factorial(int n, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	
+	return ( _pow_factorail(x^y));
+}
--- recursion/main.h
+++ recursion/main.h
@@ -6,4 +6,5 @@ int _putchar(char c);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
+ _pow_factorail(int x , int y);
 #endif /* MAIN_H */
