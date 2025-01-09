9ada401e967bce1101b3210d877e1ba6ed364d5e
fayez bast <fayezbast@gmail.com>
2025-01-09 16:44:29 +0000

    4th

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "main.h"
+
 /**
  * main - program that prints its name, followed by a new line
  * @argc: number of arguments
@@ -10,9 +10,9 @@
 
 int main(int argc, char *argv[])
 {
-
-         void(argc);
+	(void)argc;
 
 	printf("%s\n", argv[0]);
 	return (0);
 }
+
